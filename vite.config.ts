bac25395c4eb9dd63073f961f327dc83aa74c1e4
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2024-03-25 22:59:02 -0300

    feat: add support to svg files

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
