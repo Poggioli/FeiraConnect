2c4eaf45f7dded5fb7221e124c41f93ba14d4a19
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2024-03-28 16:51:25 -0300

    feat: add scroll bar to filter

--- src/components/StreetMarketsFilter/StreetMarketsFilter.tsx
+++ src/components/StreetMarketsFilter/StreetMarketsFilter.tsx
@@ -1,9 +1,10 @@
 import { Toggle } from "@/components/ui/toggle";
 import { FC } from "react";
-import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { StreetMarketsFilterProps } from "./types";
 import { useStreetMarketsFilter } from "./useStreetMarketsFilter";
-import { Skeleton } from "../ui/skeleton";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export const StreetMarketsFilter: FC<StreetMarketsFilterProps> = () => {
 
@@ -16,97 +17,100 @@ export const StreetMarketsFilter: FC<StreetMarketsFilterProps> = () => {
   } = useStreetMarketsFilter();
 
   return (
-    <div className="flex flex-row gap-4 items-center">
-      {isLoading ? (
-        <>
-          <Skeleton className="h-[1.375rem] w-28" />
-          <div className="flex flex-row gap-1 items-center">
-            {Array.from(Array(7).keys()).map((i) => <Skeleton key={i} className="h-[1.375rem] w-24" />)}
-          </div>
-        </>
-      ) : (
-        <>
-          <Toggle
-            className="whitespace-nowrap"
-            size="xsm"
-            variant="outline"
-            aria-label="Feiras abertas agora"
-            pressed={openNow}
-            onPressedChange={handleOnChangeNowOpen}
-          >
-            Abertas agora
-          </Toggle>
-          <ToggleGroup
-            type="single"
-            value={weekday}
-            onValueChange={handleOnChangeWeekday}
-          >
-            <ToggleGroupItem
+    <ScrollArea className="w-full whitespace-nowrap">
+      <div className="flex flex-row gap-4 items-center">
+        {isLoading ? (
+          <>
+            <Skeleton className="h-[1.375rem] w-28" />
+            <div className="flex flex-row gap-1 items-center">
+              {Array.from(Array(7).keys()).map((i) => <Skeleton key={i} className="h-[1.375rem] w-24" />)}
+            </div>
+          </>
+        ) : (
+          <>
+            <Toggle
               className="whitespace-nowrap"
               size="xsm"
               variant="outline"
-              value="sunday"
-              aria-label="Visualizar feiras de domingo"
+              aria-label="Feiras abertas agora"
+              pressed={openNow}
+              onPressedChange={handleOnChangeNowOpen}
             >
-              Domingo
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              className="whitespace-nowrap"
-              size="xsm"
-              variant="outline"
-              value="monday"
-              aria-label="Visualizar feiras de segunda-feira"
+              Abertas agora
+            </Toggle>
+            <ToggleGroup
+              type="single"
+              value={weekday}
+              onValueChange={handleOnChangeWeekday}
             >
-              Segunda-feira
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              className="whitespace-nowrap"
-              size="xsm"
-              variant="outline"
-              value="tuesday"
-              aria-label="Visualizar feiras de terça-feira"
-            >
-              Terça-feira
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              className="whitespace-nowrap"
-              size="xsm"
-              variant="outline"
-              value="wednesday"
-              aria-label="Visualizar feiras de quarta-feira"
-            >
-              Quarta-feira
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              className="whitespace-nowrap"
-              size="xsm"
-              variant="outline"
-              value="thursday"
-              aria-label="Visualizar feiras de quinta-feira"
-            >
-              Quinta-feira
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              className="whitespace-nowrap"
-              size="xsm"
-              variant="outline"
-              value="friday"
-              aria-label="Visualizar feiras de sexta-feira"
-            >
-              Sexta-feira
-            </ToggleGroupItem>
-            <ToggleGroupItem
-              className="whitespace-nowrap"
-              size="xsm"
-              variant="outline"
-              value="saturday"
-              aria-label="Visualizar feiras de sábado"
-            >
-              Sábado
-            </ToggleGroupItem>
-          </ToggleGroup>
-        </>
-      )}
-    </div>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="sunday"
+                aria-label="Visualizar feiras de domingo"
+              >
+                Domingo
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="monday"
+                aria-label="Visualizar feiras de segunda-feira"
+              >
+                Segunda-feira
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="tuesday"
+                aria-label="Visualizar feiras de terça-feira"
+              >
+                Terça-feira
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="wednesday"
+                aria-label="Visualizar feiras de quarta-feira"
+              >
+                Quarta-feira
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="thursday"
+                aria-label="Visualizar feiras de quinta-feira"
+              >
+                Quinta-feira
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="friday"
+                aria-label="Visualizar feiras de sexta-feira"
+              >
+                Sexta-feira
+              </ToggleGroupItem>
+              <ToggleGroupItem
+                className="whitespace-nowrap"
+                size="xsm"
+                variant="outline"
+                value="saturday"
+                aria-label="Visualizar feiras de sábado"
+              >
+                Sábado
+              </ToggleGroupItem>
+            </ToggleGroup>
+          </>
+        )}
+      </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 };
