09b366ae93805e4fdee82a17a25c4c5a043de1c2
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2024-04-03 19:39:58 -0300

    tests: add tests to getExhibitorsByStreetMarket

---
+++ __tests__/getExhibitorsByStreetMarket.mock.ts
@@ -0,0 +1,62 @@
+import { HttpResponse, http } from "msw";
+
+export const getExhibitorsByStreetMarketHandlers = {
+  filledOnePage: http.get(/^.+\/exhibitors/, () => {
+    return HttpResponse.json({
+      metadata: {
+        currentPage: 1,
+        itemsPerPage: 10,
+        totalPages: 1,
+        totalItems: 1,
+        isLastPage: true,
+      },
+      items: [
+        {
+          id: 'id',
+          name: 'name 1',
+          slug: 'name-1',
+          description: 'description',
+          whatsApp: '19912345678',
+          website: 'http://google.com.br',
+          phone: '19912345678',
+          items: ['item 1', 'item 2'],
+        },
+      ],
+    });
+  }),
+  filledMultPagesPageOne: http.get(/^.+\/exhibitors/, () => {
+    return HttpResponse.json({
+      metadata: {
+        currentPage: 1,
+        itemsPerPage: 10,
+        totalPages: 2,
+        totalItems: 11,
+        isLastPage: false,
+      },
+      items: [
+        ...Array.from(Array(10).keys()).map((item) => (        {
+          id: item,
+          name: `name ${item}`,
+          slug: `name-${item}`,
+          description: 'description',
+          whatsApp: '19912345678',
+          website: 'http://google.com.br',
+          phone: '19912345678',
+          items: ['item 1', 'item 2'],
+        })),
+      ],
+    });
+  }),
+  empty: http.get(/^.+\/exhibitors/, () => {
+    return HttpResponse.json({
+      metadata: {
+        currentPage: 1,
+        itemsPerPage: 10,
+        totalPages: 1,
+        totalItems: 0,
+        isLastPage: true,
+      },
+      items: [],
+    });
+  }),
+};
--- src/services/getExhibitorsByStreetMarket/getExhibitorsByStreetMarket.spec.ts
+++ src/services/getExhibitorsByStreetMarket/getExhibitorsByStreetMarket.spec.ts
@@ -1,25 +1,35 @@
-import { createUseGetStreetMarketsByCityQueryKey, useGetStreetMarketsByCity } from "./getStreetMarketsByCity";
+import {
+  createUseGetExhibitorsByStreetMarketQueryKey,
+  useGetExhibitorsByStreetMarket,
+} from "./getExhibitorsByStreetMarket";
 import { server } from "@tests/server.mock";
-import { getStreetMarketsByCityHandlers } from "@tests/getStreetMarketsByCity.mock";
+import { getExhibitorsByStreetMarketHandlers } from "@tests/getExhibitorsByStreetMarket.mock";
 import { customRenderHook } from "@tests/customRenderHook";
 import { waitFor } from "@testing-library/dom";
 
-describe("@/services/getStreetMarketsByCity", () => {
+describe("@/services/getExhibitorsByStreetMarket", () => {
   const slug = "slug";
 
-  it(`GIVEN a createUseGetStreetMarketsByCityQueryKey
-      WHEN call this method with { city: "slug" }
-      THEN should return ["getStreetMarketsByCity", { city: "slug" }]`, () => {
-    const result = createUseGetStreetMarketsByCityQueryKey({ city: slug });
-    expect(result).toStrictEqual(["getStreetMarketsByCity", { city: slug }]);
+  it(`GIVEN a createUseGetExhibitorsByStreetMarketQueryKey
+      WHEN call this method with { streetMarket: "slug" }
+      THEN should return ["getExhibitorsByStreetMarket", { streetMarket: "slug" }]`, () => {
+    const result = createUseGetExhibitorsByStreetMarketQueryKey({
+      streetMarket: slug,
+    });
+    expect(result).toStrictEqual([
+      "getExhibitorsByStreetMarket",
+      { streetMarket: slug },
+    ]);
   });
 
-  it(`GIVEN a createUseGetStreetMarketsByCityQueryKey
+  it(`GIVEN a createUseGetExhibitorsByStreetMarketQueryKey
       WHEN service return succes
       AND isLastPage property in metadata property is true
       THEN should NOT have other page`, async () => {
-    server.use(getStreetMarketsByCityHandlers.filledOnePage);
-    const { result } = customRenderHook(() => useGetStreetMarketsByCity({ city: slug }));
+    server.use(getExhibitorsByStreetMarketHandlers.filledOnePage);
+    const { result } = customRenderHook(() =>
+      useGetExhibitorsByStreetMarket({ streetMarket: slug })
+    );
 
     await waitFor(() => {
       expect(result.current.isLoading).toBe(false);
@@ -28,12 +38,14 @@ describe("@/services/getStreetMarketsByCity", () => {
     });
   });
 
-  it(`GIVEN a createUseGetStreetMarketsByCityQueryKey
+  it(`GIVEN a createUseGetExhibitorsByStreetMarketQueryKey
       WHEN service return succes
       AND isLastPage property in metadata property is false
       THEN should have other page`, async () => {
-    server.use(getStreetMarketsByCityHandlers.filledMultPagesPageOne);
-    const { result } = customRenderHook(() => useGetStreetMarketsByCity({ city: slug }));
+    server.use(getExhibitorsByStreetMarketHandlers.filledMultPagesPageOne);
+    const { result } = customRenderHook(() =>
+      useGetExhibitorsByStreetMarket({ streetMarket: slug })
+    );
 
     await waitFor(() => {
       expect(result.current.isLoading).toBe(false);
@@ -42,13 +54,15 @@ describe("@/services/getStreetMarketsByCity", () => {
     });
   });
 
-  it(`GIVEN a createUseGetStreetMarketsByCityQueryKey
+  it(`GIVEN a createUseGetExhibitorsByStreetMarketQueryKey
       WHEN service return succes
       AND items property is filled with at least one value
       THEN isFilled should be true
       AND isEmpty should be false`, async () => {
-    server.use(getStreetMarketsByCityHandlers.filledOnePage);
-    const { result } = customRenderHook(() => useGetStreetMarketsByCity({ city: slug }));
+    server.use(getExhibitorsByStreetMarketHandlers.filledOnePage);
+    const { result } = customRenderHook(() =>
+      useGetExhibitorsByStreetMarket({ streetMarket: slug })
+    );
 
     await waitFor(() => {
       expect(result.current.isLoading).toBe(false);
@@ -58,13 +72,15 @@ describe("@/services/getStreetMarketsByCity", () => {
     });
   });
 
-  it(`GIVEN a createUseGetStreetMarketsByCityQueryKey
+  it(`GIVEN a createUseGetExhibitorsByStreetMarketQueryKey
       WHEN service return succes
       AND items property is empty
       THEN isEmpty should be true
       AND isFilled should be false`, async () => {
-    server.use(getStreetMarketsByCityHandlers.empty);
-    const { result } = customRenderHook(() => useGetStreetMarketsByCity({ city: slug }));
+    server.use(getExhibitorsByStreetMarketHandlers.empty);
+    const { result } = customRenderHook(() =>
+      useGetExhibitorsByStreetMarket({ streetMarket: slug })
+    );
 
     await waitFor(() => {
       expect(result.current.isLoading).toBe(false);
